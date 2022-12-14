f9b3db6ce645f9df290aef7ada0c5f1a73fa9b7e
Erick Roy Fernandez Terrel <efernandez@truenorth.pr>
2022-12-14 16:12:47 -0500

    Initial Commit

---
+++ CalculatorApp/CalculatorAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatorAppApp.swift
+//  CalculatorApp
+//
+//  Created by Erick Roy Fernandez Terrel on 14/12/22.
+//
+
+import SwiftUI
+
+@main
+struct CalculatorAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
